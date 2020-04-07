559aaf893749be7c32fbc9de1b582a022a56edf9
n.shiraishi <20170929friday@gmail.com>
2020-04-07 07:11:43 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -40,6 +40,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->status = $request->status;
@@ -83,6 +88,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
         $task->status = $request->status;
